cf34e8f9cd1e29a630529a1bd206fd1a6f2834b5
Thomas Dethorey <thomas.dethorey01@gmail.com>
2018-10-14 15:04:52 +0200

    ex 3

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// var prenom = prompt("Entrez votre prénom :");
+// alert("Bonjour, " + prenom);
+
+var firstName = prompt("Entrez votre prénom :");
+if(isNaN(firstName)){
+alert("Bonjour, " + firstName);
+}else{
+alert("Erreur");
+};
